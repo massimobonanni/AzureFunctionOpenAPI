ac5884485a1a48da449ca424d5945d4bc3536cae
Massimo Bonanni <mabonann@microsoft.com>
2022-01-22 11:02:27 +0100

    Advanced configuration added

---
+++ AzureFunctionOpenAPI/OpenApi/OpenApiConfigurationOptions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFunctionOpenAPI.OpenApi
+{
+    public class OpenApiConfigurationOptions : DefaultOpenApiConfigurationOptions
+    {
+         public override OpenApiInfo Info { get; set; } = new OpenApiInfo()
+        {
+            Version = "1.0.0",
+            Title = "Awesome Weather REST API",
+            Description = "Awesome Weather provides the best API to get updated and accurate weather forecasts. the",
+            TermsOfService = new Uri("https://github.com/massimobonanni/AzureFunctionOpenAPI"),
+            Contact = new OpenApiContact()
+            {
+                Name = "AwesomeWeather",
+                Email = "info@awesomeweather.com",
+                Url = new Uri("https://github.com/massimobonanni/AzureFunctionOpenAPI"),
+            },
+            License = new OpenApiLicense()
+            {
+                Name = "MIT",
+                Url = new Uri("http://opensource.org/licenses/MIT"),
+            }
+        };
+    }
+}
